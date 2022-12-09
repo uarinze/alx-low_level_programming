852e603177d3db2e6ab5bbd5e26dce43349ce3f5
Ugo <alexarinzeugo@yahoo.com>
2022-12-08 18:39:41 -0800

    j

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-/*
- * *prints some character
- * */
+/* prints some character */
+ 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
