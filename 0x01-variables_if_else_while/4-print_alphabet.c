706114e5eb2697d23108b21d2f049850fa7abb3a
Ugo <alexarinzeugo@yahoo.com>
2022-12-09 19:19:51 -0800

    add file 4

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * prints alphabet in reverse order
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 97; c <= 122; c++)
+	{
+		if (c != 101 && c != 113)
+		       	putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
