d115883a69ae66c306b10a1308d58ebb21f72fdb
Ugo <alexarinzeugo@yahoo.com>
2022-12-10 04:31:04 -0800

    add file 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/**
+ * main - Entry point
+ *
+ * Prints combination of two digits
+ *
+ * Return - 0 (Success)
+ **/
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i = 57; i++)
+	{
+		for (j = 48; j = 57; j++)
+		{
+			if (i != j && i < j)
+			{
+				putchar(i);
+				putchar(j);
+				if (i != 56 && j != 57)
+				{
+					putchar(44);
+					putchar(20);
+				}
+			}
+		}
+	}
+	putchar(10);
+}
