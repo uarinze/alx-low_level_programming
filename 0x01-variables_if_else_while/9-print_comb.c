d2328b7242fdf1a89ce17ac7da101d58fd61ec36
Ugo <alexarinzeugo@yahoo.com>
2022-12-10 04:02:34 -0800

    add file9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * prints single numbers, comma, and space using ascii characters
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);/* prints the number */
+		putchar(44);/* adds a comma*/
+		putchar(32);/* adds a space*/
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
