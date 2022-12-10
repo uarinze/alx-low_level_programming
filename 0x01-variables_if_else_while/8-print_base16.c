0a49f2979633c17eee4e3cdfc91fb7c78c9a8eea
Ugo <alexarinzeugo@yahoo.com>
2022-12-09 19:58:44 -0800

    add fie 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * prints hexadecimal numbers
+ *
+ * Return 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 1; i >=15 ; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
+
