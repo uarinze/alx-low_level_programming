15f01d63caa9b47e80944e2f3120cd6dec3fbe60
Ugo <alexarinzeugo@yahoo.com>
2022-12-09 19:34:49 -0800

    add file6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * prints alphabet in reverse order
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char j;
+
+	for (j = 48; j <= 57; j++)
+		putchar(j);
+
+	putchar('\n');
+
+	return (0);
+}
