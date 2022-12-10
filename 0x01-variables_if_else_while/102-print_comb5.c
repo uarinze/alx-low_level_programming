1db4824550f3a84c350c5effe0a998c10138bb2a
Ugo <alexarinzeugo@yahoo.com>
2022-12-10 08:13:15 -0800

    ll

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -22,7 +22,7 @@ int main(void)
 			{
 				for (l = 48; l <= 57; l++)
 				{
-					if (i + j < k + l && i <= k)
+					if (i <= k && j != 32 && l != 32)
 					{
 						putchar(i);/*adds first digit of first pair*/
 						putchar(j);/*adds second digit of first pair*/
