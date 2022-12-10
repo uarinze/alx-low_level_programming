4a426c586365369884c8351353ccbc6f1e79cd09
Ugo <alexarinzeugo@yahoo.com>
2022-12-10 04:32:48 -0800

    edit file 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,9 +14,9 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 48; i = 57; i++)
+	for (i = 48; i <= 57; i++)
 	{
-		for (j = 48; j = 57; j++)
+		for (j = 48; j <= 57; j++)
 		{
 			if (i != j && i < j)
 			{
