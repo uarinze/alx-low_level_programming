81425459981c41d69d6268913908cd2b2a69ba0b
Ugo <alexarinzeugo@yahoo.com>
2022-12-10 04:56:51 -0800

    add file 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/**
+ * main - Entry point
+ *
+ * Prints combination of three digits
+ *
+ * Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				if (i != j && i < j && j != k && j < k && i !=k && i < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i + j + k != 168)
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
+	}
+	putchar(10);
+
+	return (0);
+}
