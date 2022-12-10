5488f73a63631646b161303abf721d898f3f3f76
Ugo <alexarinzeugo@yahoo.com>
2022-12-09 17:53:10 -0800

    edit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 		printf("and is greater than 5\n");
 	else if (n == 0)
 		printf("and is 0\n");
-	else if (n < 6 && != 0) 
+	else if (n < 6 && n != 0) 
 		printf("and is less than 6 and not 0\n");
 	else
 		printf("\n");
