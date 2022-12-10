6c903c21c49cb97870a37474dcc454dd6e50029c
Ugo <alexarinzeugo@yahoo.com>
2022-12-10 09:44:05 -0800

    add file 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Prints to standard error
+ *
+ * Reurns: 1 (Unsuccessful) 
+ **/
+
+int main(void)
+{
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+
+	1&>2;
+
+	return 1;
+}
