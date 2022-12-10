bae7a14b6c1538f59f3fb8ee185bf11d11d81f70
Ugo <alexarinzeugo@yahoo.com>
2022-12-09 20:30:28 -0800

    ff

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,7 +6,7 @@
  *
  * prints hexadecimal numbers
  *
- * Return 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main (void)
@@ -14,7 +14,7 @@ int main (void)
 	char i;
 
 	for (i = 1; i >= 15; i++)
-		putchar('i');
+		putchar(i);
 	putchar('\n');
 
 	return (0);
