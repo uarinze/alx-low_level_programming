30aa8d94cc9fa9bf26adaf98eb8682a8dc280c3a
Ugo <alexarinzeugo@yahoo.com>
2022-12-09 19:29:27 -0800

    add file 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * prints alphabet in reverse order
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+
+	putchar('\n');
+
+	return (0);
+}
