19a3314eade90068f523885e648a4c502a0be044
Ugo <alexarinzeugo@yahoo.com>
2022-12-13 17:07:11 -0800

    update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -15,9 +15,23 @@ void print_putchar(void)
 
 void print_alphabet(void)
 {
-	int p_a[] = {97 .. 122, 65 .. 90};
+	int p_a[52];
 
-	int i;
+	int a, b, c, d, i;
+
+	b = 0;
+	d = 26;
+
+	for (a = 97; a <= 122; a++)
+	{
+		p_a[b] = a;
+		b++;
+	}
+	for (c = 65; c <= 90; c++)
+	{
+		p_a[d] = c;
+		d++;
+	}
 
 	for (i = 0; i < 52; i++)
 	{
