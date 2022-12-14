0b9774eb3e26509df47ed4d613b425ed87d2c31f
Ugo <alexarinzeugo@yahoo.com>
2022-12-13 17:55:23 -0800

    add 2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-main.h
@@ -0,0 +1,29 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdio.h>
+
+void print_alphabet_x10(void)
+{
+	int p_a[26];
+
+	int a, b, i, j;
+
+	b = 0;
+	
+	for (a = 97; a <= 122; a++)
+	{
+		p_a[b] = a;
+		b++;
+	}
+	for (j = 1; j <=10; j++)
+	{
+		for (i = 0; i < 26; i++)
+		{
+			putchar(p_a[i]);
+		}
+		putchar(10);
+	}
+}
+
+#endif
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "2-main.h"
+/**
+ * main - entry point
+ *
+ * prints alphabets in lowercase ten times
+ *
+ * Return: 0 (Success)
+ *
+ **/
+
+int main(void)
+{
+	print_alphabet_x10();
+
+	return (0);
+}
