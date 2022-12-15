33ffe91e0fdc5464c9200e9864c7d9e4bf2ecf0f
Ugo <alexarinzeugo@yahoo.com>
2022-12-14 18:29:18 -0800

    add file 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs: - prints the sign of a number
+ * @n:- collects the number to test
+ *
+ * Description: it calculates the absolut value
+ * of a number.
+ * Return: It returns the absolute value of a number.
+ **/
+
+int _abs(int n)
+{
+	int i;
+	
+	i = (n*n)/n;
+	return (i);
+}
