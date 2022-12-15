f0376f8a7844e68e8cdbf2a5fd890aec538e8afa
Ugo <alexarinzeugo@yahoo.com>
2022-12-15 00:24:51 -0800

    add file 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of any number
+ * @n: - takes the value to be evaluated
+ *
+ * Description: it takes an integer value and
+ * prints the last digit.
+ * Return: It returns the absolute value of a number.
+ **/
+int print_last_digit(int n)
+{
+
+	int j = n;
+
+	int i;
+
+	i = j % 10;
+	return (i);
+}
