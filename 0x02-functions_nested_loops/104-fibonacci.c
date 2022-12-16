e2bb81fc155a9ebbd08fc944b92bdaa5e36e2078
Ugo <alexarinzeugo@yahoo.com>
2022-12-15 16:43:48 -0800

    edit 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - find and prints the first 98
  * fibonacci numbers starting with 1 and 2
@@ -11,48 +10,38 @@ int main(void)
 	int i;
 	unsigned long f1 = 0;
 	unsigned long f2 = 1;
-	unsigned long sum;
-	unsigned long h1;
-	unsigned long h2;
-	unsigned long f1_h1;
-	unsigned long f1_h2;
-	unsigned long f2_h1;
-	unsigned long f2_h2;
-	for (i = 0; i < 98; i++)
+	unsigned long sum, h1, h2, f1_h1, f1_h2, f2_h1, f2_h2;
+
+	for(i = 0; i < 98; i++)
 	{
 		sum = f1 + f2;
 		printf("%lu,  ", sum);
-
 		f1 = f2;
 		f2 = sum;
 	}
-
 	f1_h1 = f1 / 10000000000;
 	f2_h1 = f2 / 10000000000;
 	f1_h2 = f1 % 10000000000;
 	f2_h2 = f2 % 10000000000;
 
-	for (i = 93; i < 99; i++)
+	for(i = 93; i < 99; i++)
 	{
 		h1 = f1_h1 + f2_h1;
 		h2 = f1_h2 + f2_h2;
 
-		if (f1_h2 + f2_h2 > 9999999999)
+		if(f1_h2 + f2_h2 > 9999999999)
 		{
 			h1 += 1;
 			h2 %= 10000000000;
 		}
 		printf("%lu%lu", h1, h2);
-		if (i != 98)
+		if(i != 98)
 			printf (", ");
-
 		f1_h1 = f2_h1;
 		f1_h2 = f2_h2;
 		f2_h1 = h1;
 		f2_h2 = h2;
 	}
 	printf("\n");
-	
 	return (0);
-
 }
