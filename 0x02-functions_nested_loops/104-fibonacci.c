3d8dba5752dc763973830612c7dd48990cb8e962
Ugo <alexarinzeugo@yahoo.com>
2022-12-15 16:47:35 -0800

    more edits

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,7 +12,7 @@ int main(void)
 	unsigned long f2 = 1;
 	unsigned long sum, h1, h2, f1_h1, f1_h2, f2_h1, f2_h2;
 
-	for(i = 0; i < 98; i++)
+	for (i = 0; i < 92; i++)
 	{
 		sum = f1 + f2;
 		printf("%lu,  ", sum);
@@ -24,19 +24,19 @@ int main(void)
 	f1_h2 = f1 % 10000000000;
 	f2_h2 = f2 % 10000000000;
 
-	for(i = 93; i < 99; i++)
+	for (i = 93; i < 99; i++)
 	{
 		h1 = f1_h1 + f2_h1;
 		h2 = f1_h2 + f2_h2;
 
-		if(f1_h2 + f2_h2 > 9999999999)
+		if (f1_h2 + f2_h2 > 9999999999)
 		{
 			h1 += 1;
 			h2 %= 10000000000;
 		}
 		printf("%lu%lu", h1, h2);
-		if(i != 98)
-			printf (", ");
+		if (i != 98)
+			printf(", ");
 		f1_h1 = f2_h1;
 		f1_h2 = f2_h2;
 		f2_h1 = h1;
