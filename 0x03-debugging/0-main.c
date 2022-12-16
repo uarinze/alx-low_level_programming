f26c0c18704328f83cfce6cd965b53d7166e59ea
Ugo <alexarinzeugo@yahoo.com>
2022-12-16 00:48:16 -0800

    add file 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *
+ ** main - tests function that prints if integer is positive or negative
+ *
+ ** Return: 0
+ *
+ **/
+
+int main(void)
+
+{
+
+	int i;
+
+	i = 98;
+	
+	if (i == 0) printf("%d is zero", i);
+	else positive_or_negative(i);
+	
+	return (0);
+}
