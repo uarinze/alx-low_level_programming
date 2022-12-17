3c7e59fb61662a150f86c0b8ab365bf67c2c1a86
Ugo <alexarinzeugo@yahoo.com>
2022-12-16 19:13:22 -0800

    add file 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - prints the square shape
+ * @size: first argument
+ *
+ * Description: it prints a square and returns no value
+ */
+void print_square(int size)
+{
+	int dim1; dim2;
+
+	if (size > 0)
+	{
+		for (dim1 =0; dim1 < size; dim1++)
+		{
+			for (dim2 = 0; dim2 < size; dim2++)
+				_putchr(35);
+			if (dim1 == size -1)
+				continue;
+			_putchar(10);
+		}
+	}
+	_putchar(10);
+}
+
