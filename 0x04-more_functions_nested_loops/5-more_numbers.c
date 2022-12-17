48e3d2ab26b0d5ccd5c27fa5d72dbc56971db145
Ugo <alexarinzeugo@yahoo.com>
2022-12-16 18:46:24 -0800

    add file 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+				_putchar((j / 10) + 48);
+			_putchar((j % 10) + 48);
+		}
+		_putchar(10);
+	}
+}
