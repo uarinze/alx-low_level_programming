18e4825f30b9fb70b4c0aca7039c4d2152ec7d49
Ugo <alexarinzeugo@yahoo.com>
2022-12-16 16:08:07 -0800

    mm

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -17,8 +17,10 @@ void print_remaining_days(int month, int day, int year)
 		{
 			day++;
 		}
-		else if (month <= 2 && day <= 60)
-			day = day;
+		else
+		{
+			break;
+		}
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
