1c4e9fd06f61b01d64e9d9939b351ddf5c7cc2d9
Ugo <alexarinzeugo@yahoo.com>
2022-12-16 18:38:00 -0800

    g

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,12 +12,8 @@ void print_most_numbers(void)
 
 	for (i = 48; i <= 57; i++)
 	{
-		if (i == 50 || i == 53)
-			continue;
-		else
-			break;
-		}
-		_putchar(i);
+		if (i != 50 && i != 52)
+			_putchar(i);
 	}
 	_putchar(10);
 }
