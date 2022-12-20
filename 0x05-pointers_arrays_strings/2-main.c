8dfbfe5fd9a5454c0b7ebb809b6c6dbc5db76574
Ugo <alexarinzeugo@yahoo.com>
2022-12-20 01:30:45 -0800

    edit

---
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int main(void)
+{
+	char *str;
+	int len;
+
+	str = "My first strlen!";
+	len = _strlen(str);
+	return (0);
+}
--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string
- *@s: - a pointer as an argument
+ * _strlen: - returns the length of a string
+ *@s: - string argument
  *
  * Description: this function takes a pointer to a char
  * as parameter and returns the length of the string.
@@ -13,9 +13,9 @@ int _strlen(char *s)
 
 	int i = 0;
 
-	while (*(s + i) != 0);
+	while (*(s + i) != '\0');
 	{
-		i++
+		i++;
 	}
 
 	return (i);
