8a741c12fed5f0a4eeccf046d47c5dadefa35dc1
Ugo <alexarinzeugo@yahoo.com>
2022-12-20 15:37:25 -0800

    add file 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src,
+ * including the null byte, to the buffer pointed
+ * to by dest.
+ * @dest: - first argument which is where we copy the string to
+ * @src: - second argumet which is the source string to be copied.
+ *
+ * Description: this function copies string from one
+ * locatino to another.
+ * Return: It returns the copied string.
+ */
+void _strcpy(char *dest, char *src)
+{
+	dest = src;
+
+	return (*dest);
+
+}
