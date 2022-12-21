fa25b1dc2ce467706609fb31b0a55a9df8b35c31
Ugo <alexarinzeugo@yahoo.com>
2022-12-20 18:52:06 -0800

    -add file 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *  _atoi - converts a string to an integer
+ *  @s: first argument
+ *
+ *  Description: it converst a string to integer
+ *  and returs the integer.
+ *  Return: the integer that was converted from the string.
+ */
+int _atoi(char *s)
+{
+	int i = 1;
+	int j = 0;
+
+	while (*s++)
+	{
+		if (*s == '-')
+			i = i * -1;
+		else if (*s >= 48 && *s <= 57)
+			j = (j * 10) + (*s - 48);
+		else if (j > 0)
+			break;
+	}
+	return (j * i);
+
+
+}
