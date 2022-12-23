2b46c5c921875076dfcfa00b619be5a032c5a4b8
Ugo <alexarinzeugo@yahoo.com>
2022-12-22 18:47:09 -0800

    add file4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,7 +12,7 @@ void reverse_array(int *a, iny n)
 	int b;
 	int i;
 
-	for ( i = n - 1; i >= n / 2; i --)
+	for ( i = (n - 1); i >= (n / 2); i --)
 	{
 		b = a[(n - 1 - i)];
 		a[(n - 1 - i)] = a[i];
