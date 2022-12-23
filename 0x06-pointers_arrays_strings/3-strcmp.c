ad7d28b488a338f3a5c264d9295a28df7e8a3b31
Ugo <alexarinzeugo@yahoo.com>
2022-12-22 19:03:28 -0800

    edit file 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,12 +4,12 @@
  * _strcmp - compares two strings.
  * @s1 - first argument
  * @s2 - second argumet
- * 
+ *
  * Description: this function compares two strings
  * it returns an integer less tahn, equal to, or greater than zero
  * if s1 is found, respectively, to be less than, to match, or
  * be greater tah s2.
- * 
+ *
  * Return: It returns an int. .
  */
 int _strcmp(char *s1, char *s2)
