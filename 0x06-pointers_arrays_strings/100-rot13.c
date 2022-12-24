ad9ce4d39429fbc07d1e249ba3984888064b4b5c
Ugo <alexarinzeugo@yahoo.com>
2022-12-23 17:36:51 -0800

    add file 100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *rot13 - encoes a string in rot13
+ *@str: string to encode
+ *
+ *description: encodes a string to rot13
+return: returns an encoded string
+ */
+char *rot13(char *str)
+{
+	int i;
+	int j;
+
+	char a[52] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+	char b[52] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == a[j])
+			{
+				str[i] = b[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
