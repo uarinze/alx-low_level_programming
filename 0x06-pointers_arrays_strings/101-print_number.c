2529db6bc6fddca5bf055a2be8c2e47d4dac8037
Ugo <alexarinzeugo@yahoo.com>
2022-12-23 17:49:30 -0800

    edit101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,7 +8,7 @@
  */
 void print_number(int n)
 {
-	int i = n;
+	unsigned int i = n;
 
 	if (n < 0)
 	{
@@ -17,7 +17,7 @@ void print_number(int n)
 	}
 	if ((i / 10) > 0)
 	{
-		_putchar((i / 10) + '0');
-		_putchar((i % 10) + '0');
+		print_number(i / 10);
 	}
+	_putchar((i % 10) + 48);
 }
