b61f359d0993cdf556019c2d49d819326d363516
Ugo <alexarinzeugo@yahoo.com>
2023-01-02 15:23:36 -0800

    edit file1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -14,10 +14,12 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 	unsigned int j = 0;
+	unsigned char *d = dest;
+	unsigned char *s = src;
 
 	for (i = 0; i <= n; i++)
 	{
-		*(dest + j) = *(src + i);
+		*(d + j) = *(s + i);
 		j++;
 	}
 
