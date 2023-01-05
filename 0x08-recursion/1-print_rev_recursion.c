be08bc47bd4aeb52d33e84e8ea9f009e5f4b5028
Ugo <alexarinzeugo@yahoo.com>
2023-01-04 16:35:30 -0800

    add file 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * follwed by a new line
+ * @s: - string to print
+ *
+ * Desctription: prints a string in reverse follwed by \n
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
