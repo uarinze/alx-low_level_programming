c3970819405f7b0f7e070dc2e5d138d4b273159a
Ugo <alexarinzeugo@yahoo.com>
2023-01-06 17:39:45 -0800

    edit01

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-gcc -c -Wall -Werror -Wextra -pedantic -std=gnu89 *.c | ar rc liball.a | ranlib 
+gcc -c -Wall -Werror -Wextra -pedantic -std=gnu89 *.c | ar rc liball.a
+ranlib liball.a
