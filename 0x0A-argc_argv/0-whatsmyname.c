ae9045a023269913f622169a3e369489d415bb35
Ugo <alexarinzeugo@yahoo.com>
2023-01-06 18:18:20 -0800

    edit file0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,9 @@
  *
  * Return: - return 0 (Success)
  */
-int main(int argc, char **argv)
+int main(int __attribute__((__unused__)) argc, char **argv)
 {
-	printf("%s\n", argv[argc[0]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
