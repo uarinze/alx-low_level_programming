2063674b0cc0a185da2b0ea868c7bf7a3b5238a4
Ugo <alexarinzeugo@yahoo.com>
2023-01-06 18:13:30 -0800

    add file0 and rdme

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints its name
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: - return 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[argc[0]);
+
+	return (0);
+}
