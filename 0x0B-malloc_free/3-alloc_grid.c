ef552a153a3fea27b326902543277313a3439639
Ugo <alexarinzeugo@yahoo.com>
2023-01-11 04:27:39 -0800

    edit file 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,4 +11,25 @@
  */
 int **alloc_grid(int width, int height)
 {
+	int **matrix;
+	int i, j;
 
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	
+	matrix = (int **)malloc(height * width *(sizeof(int **)));
+	if (matrix == 0)
+		return (NULL);
+	else{
+	for (i = 0; i < width; i++)
+	{
+		for (j = 0; j < height; j++)
+		{
+			matrix[i][j] = 0;
+/*			*(matrix + ((i * width + j) * sizeof(int))) = 0;*/
+		}
+	}
+	return (matrix);
+	}
+	free(matrix);
+}
