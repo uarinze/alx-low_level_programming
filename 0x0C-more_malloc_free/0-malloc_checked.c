984c6486d3f44770e4859cdbdadf838788febc29
Ugo <alexarinzeugo@yahoo.com>
2023-01-13 08:27:02 -0800

    add first files

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memorry using malloc
+ * @b: - amount of space to allocate.
+ *
+ * Description: - allocates memory using malloc
+ * Return: returns a pointer to the allocated memory
+ * if malloc fails, malloc_checked should return 98
+ * and cause normal process termination.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == 0)
+	{
+		exit(98);
+	}
+
+	return(ptr);
+}
