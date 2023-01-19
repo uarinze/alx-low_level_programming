531afff38d002aefd23f3c747680f1df7372cd90
Ugo <alexarinzeugo@yahoo.com>
2023-01-19 03:31:18 -0800

    edit file 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,9 +11,10 @@
  */
 void array_iterator(int *array, size_t size, void(*action)(int))
 {
-	long unsigned int i;
+	int i;
+	int j = (int)size;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < j; i++)
 	{
 		action(*(array + i));
 	}
