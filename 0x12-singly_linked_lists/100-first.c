acbfa86e98aca416023a30c9f3a2925367fdab26
Ugo <alexarinzeugo@yahoo.com>
2023-01-28 00:43:34 +0000

    add file100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void fun(void) __attribute__((constructor));
+
+/**
+ * fun - prints before main is called
+ */
+void fun(void)
+{
+	printf("You're beat! and yet, you must allow,\
+			\nI bore my house upon my back!\n");
+}
