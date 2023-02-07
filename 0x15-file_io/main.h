1c90494633910942f860c33d44e7f3932f136ff3
Ugo <alexarinzeugo@yahoo.com>
2023-02-07 23:46:57 +0000

    edit main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stddef.h>
-#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
