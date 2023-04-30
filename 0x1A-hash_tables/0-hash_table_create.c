d9262ed018b4b08b24aa82040a103fa0ea3568dc
Ugo <alexarinzeugo@yahoo.com>
2023-04-30 08:46:45 +0000

    handled excess memory

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
 
-	hash_table_t *ht = malloc(sizeof(hash_table_t) * size);
+	hash_table_t *ht = malloc(sizeof(hash_table_t));
 
 	if (ht == NULL)
 		return (NULL);
