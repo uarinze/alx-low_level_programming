3c52d3dfecfc807923c8e3e5f74ed3869d91ce17
Ugo <alexarinzeugo@yahoo.com>
2023-05-01 21:29:33 +0000

    add file 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,17 +10,18 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *node;
+	unsigned long int index;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
-	unsigned long int index = key_indey((const unsigned char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 
 	if (index >= ht->size)
 		return (NULL);
 
 	node = ht->array[index];
-	while (node && strcmp(node->key, key != 0))
+	while (node && strcmp(node->key, key) != 0)
 		node = node->next;
 	if (node == NULL)
 		return (NULL);
